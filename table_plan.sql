3b2d92bb4ca74042befcb820fd3476d32d643d06
hiori kino <kino.hiori@nims.go.jp>
2015-01-13 20:04:03 +0900

    add datetime in db6.py , table_plan.sql

--- table_plan.sql
+++ table_plan.sql
@@ -1,2 +1,2 @@
-create table sio2(hash,dirname,material,dbstatus,count,execstatus);
+create table sio2(hash,dirname,material,dbstatus,count,execstatus,time_last,time_init, time_send, time_recv,time_update);
 .exit
